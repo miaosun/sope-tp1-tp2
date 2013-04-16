e6da12a582afc049639edc73650026b92487d9f8
answersun <miaosun88@gmail.com>
2013-04-16 22:00:34 +0000

    printf("testtesttest: %s\n", direntp->d_name); 				if (lstat(direntp->d_name, &stat_buf)==-1) 				{ 					perror("lstat ERROR"); 					exit(3); 				}
    da erro esta parte

--- trunk/rstr.c
+++ trunk/rstr.c
@@ -12,46 +12,90 @@
 #define BUFFER_SIZE 1024
 //#define PATH_MAX 
 
-DIR *d2, *d3;
+DIR *d2, *d3, *d4;
+
+
+
+char* dir2;
+char* dir3;
+
+
+void fileCopy(char* path, char* nome_pasta) {
+
+			int fd1, fd2, nr, nw;
+			unsigned char buffer[BUFFER_SIZE]; 
+
+			fd1 = open(path, O_RDONLY); 
+			if (fd1 == -1) {
+				perror(dir1); 
+				exit(5); 
+			}
+			
+			//cria pathname do ficheiro a copiar
+			char filePath[PATH_MAX];
+			sprintf(filePath, "%s/%s/%s", dir2,nome_pasta, path);
+			
+	
+			fd2 = open(filePath, O_WRONLY | O_CREAT | O_EXCL, 0644);
+			if (fd2 == -1) {
+				perror(dir2); 
+				close(fd1);
+				exit(5);
+			} 
+			while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0) 
+				if ((nw = write(fd2, buffer, nr)) <= 0 || nw != nr) { 
+					perror(dir2); 
+					close(fd1); 
+					close(fd2); 
+					exit(6); 
+				}
+
+			/*teste erro*///write(fd_info, direntp->d_name, sizeof(dirent->d_name)); //falta /n?
+			/*teste erro*///write(infoPath, stat_buf.st_mtime, sizeof(stat_buf.st_mtime));
+			/*teste erro*///write(infoPath, dirPath, sizeof(dirPath));
+			close(fd1); 
+			close(fd2); 
+
+}
 
 int main(int argc, char* argv[]) {
-/*
-	dir1=argv[1];
-	dir2=argv[2];
-	dt = atoi(argv[3]);
-*/
+
+	dir2=argv[1];
+	dir3=argv[2];
+
 	//teste utilização
 	if(argc!=3){
 		printf("usage: %s d1 d2\n",argv[0]);
 		exit(1);
 	}
 
-	
+	struct dirent **namelist;
+	int n, i;
 	//abre directório (dir2) onde contem os backups todos e mostrar todos os backups que existem
-	if ((d2 = opendir( argv[1])) != NULL)
+	if ((d2 = opendir(dir2)) != NULL)
 	{
 		/*while((dirent = readdir(d2)) != NULL)      // isto aqui mostra conteudos nao por ordem, tipo random
 		{
 			if(dirent->d_type == DT_DIR)
 				if((strcmp(dirent->d_name, ".") != 0) && strcmp(dirent->d_name, "..") != 0)
 					printf("%s\n", dirent->d_name);			
-		}*/
-		struct dirent **namelist;
-		int n, i;
-		n = scandir(argv[1], &namelist, NULL, alphasort);
+		}*/		
+		
+		n = scandir(dir2, &namelist, NULL, alphasort);
 		if(n<0)
 			perror("scandir");
 		else
 		{
+			printf("List of avaliable restore points\n");
 			for(i=0; i<n; i++)
 			{
 				if((strcmp(namelist[i]->d_name, ".") != 0) && strcmp(namelist[i]->d_name, "..") != 0)
 				{
-					printf("%s\n", namelist[i]->d_name);
-					free(namelist[i]);
+					printf("\t%s\n", namelist[i]->d_name);
+					//free(namelist[i]);
 				}
 			}
-			free(namelist);
+			//free(namelist);
 		}
 		closedir(d2);
 	}
@@ -60,57 +104,62 @@ int main(int argc, char* argv[]) {
 		perror("argv[1]");
 		exit(4);
 	}
-	//cria directório de restore (d3)
-	//int mkdir(const char *pathname, mode_t mode);
-/*	if((mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | EEXIST))==-1) {
-		perror(argv[2]);
+
+//////////////
+
+	//cria directório de restore (dir3)
+	int mkdir(const char *pathname, mode_t mode);
+	if((mkdir(dir3, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | EEXIST))==-1) {
+		perror(dir3);
 		exit(3);
-	}*//*
+	}
 	//abre directório de backup (d3)
-	if ((d3 = opendir( argv[2])) == NULL) { 
-		perror(argv[2]);
+	if ((d3 = opendir(dir3)) == NULL) { 
+		perror(dir3);
 		exit(2); 
-	}*/
-	//mostrar todos os backups que existem no directorio dir2
-
-
-/*
-
-
-	//OU USAR SPRINTF!!!
-	if(chdir(argv[2])==-1) {
-		perror(argv[2]);
-		exit(4);
-	}
-	//criar pasta backup
-	if((mkdir(nome_pasta, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | EEXIST))==-1) {
-		perror(nome_pasta);
-		exit(4);
-	}
-	closedir(d2);
-
-
-	*/
-
-	
-/*
-	//cria ficheiro __bckpinfo__
-	char infoPath[PATH_MAX];
-	//if((strlen(argv[2])-1)=='/')
-	//	sprintf(infoPath, "%s%s/%s", argv[2],nome_pasta, "__bckpinfo__");
-	//else
-		sprintf(infoPath, "%s/%s/%s", argv[2],nome_pasta, "__bckpinfo__");
-
-	int fd_info;
-	if((fd_info=open(infoPath, O_WRONLY | O_CREAT | O_EXCL | O_APPEND, 0644))==-1) {
-	perror(infoPath);
-	exit();
 	}
 
-	}
+//////////////////
 
-	closedir(d1);
-*/
+	char op_dir[BUFFER_SIZE];
+	printf("\nWhich restore point (time) ?\n");
+	scanf("%s", op_dir);
+
+	for(i=0; i<n; i++)
+	{
+		if(strcmp(namelist[i]->d_name, op_dir) == 0)
+		{
+			//chdir(argv[1]);
+			struct dirent *direntp; 
+			struct stat stat_buf;
+			char path1[PATH_MAX];
+			sprintf(path1, "%s/%s", dir2,op_dir);
+			if ((d4 = opendir(path1)) == NULL) { 
+				perror(op_dir);
+				exit(2); 
+			}
+
+			while ((direntp = readdir(d4)) != NULL) 
+			{ 
+				printf("testtesttest: %s\n", direntp->d_name);
+				if (lstat(direntp->d_name, &stat_buf)==-1)
+				{
+					perror("lstat ERROR");
+					exit(3);
+				}
+		
+				if (S_ISREG(stat_buf.st_mode))
+					fileCopy(direntp->d_name, argv[2]);		
+			}
+
+			closedir(d4);
+			break;
+		}
+	}
+	//closedir(d2);
+	free(namelist);
+
+
 	printf("\nFinishing!\n\n");
 	exit(0);
 
