1c99e4cd3ae866e643f2740c6d1bc41211ddcbd1
answersun <answersun@f10c690a-065e-4c8b-b573-574a2598cf45>
2013-04-17 16:22:37 +0000

    funcoes.h

--- trunk/rstr.c
+++ trunk/rstr.c
@@ -4,12 +4,8 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <time.h>
-#include <fcntl.h> //open files
-
-#define BUFFER_SIZE 1024
+#include <wait.h>
+#include "funcoes.h"
 //#define PATH_MAX 
 
 DIR *d2, *d3, *d4;
@@ -19,43 +15,6 @@ char* dir2;
 char* dir3;
 
 
-void fileCopy(char* path, char* dir3) {
-
-			int fd1, fd2, nr, nw;
-			unsigned char buffer[BUFFER_SIZE]; 
-
-			fd1 = open(path, O_RDONLY); 
-			if (fd1 == -1) {
-				perror(path); 
-				exit(5); 
-			}
-			
-			//cria pathname do ficheiro a copiar
-			char filePath[PATH_MAX];
-			sprintf(filePath, "%s/%s", dir3,path);
-			
-	
-			fd2 = open(filePath, O_WRONLY | O_CREAT | O_EXCL, 0644);
-			if (fd2 == -1) {
-				perror(filePath); 
-				close(fd1);
-				exit(5);
-			} 
-			while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0) 
-				if ((nw = write(fd2, buffer, nr)) <= 0 || nw != nr) { 
-					perror(dir3); 
-					close(fd1); 
-					close(fd2); 
-					exit(6); 
-				}
-
-			/*teste erro*///write(fd_info, direntp->d_name, sizeof(dirent->d_name)); //falta /n?
-			/*teste erro*///write(infoPath, stat_buf.st_mtime, sizeof(stat_buf.st_mtime));
-			/*teste erro*///write(infoPath, dirPath, sizeof(dirPath));
-			close(fd1); 
-			close(fd2); 
-
-}
 
 int main(int argc, char* argv[]) {
 
@@ -73,13 +32,6 @@ int main(int argc, char* argv[]) {
 	//abre directório (dir2) onde contem os backups todos e mostrar todos os backups que existem
 	if ((d2 = opendir(dir2)) != NULL)
 	{
-		/*while((dirent = readdir(d2)) != NULL)      // isto aqui mostra conteudos nao por ordem, tipo random
-		{
-			if(dirent->d_type == DT_DIR)
-				if((strcmp(dirent->d_name, ".") != 0) && strcmp(dirent->d_name, "..") != 0)
-					printf("%s\n", dirent->d_name);			
-		}*/		
-		
 		n = scandir(dir2, &namelist, NULL, alphasort);
 		if(n<0)
 			perror("scandir");
@@ -91,10 +43,8 @@ int main(int argc, char* argv[]) {
 				if((strcmp(namelist[i]->d_name, ".") != 0) && strcmp(namelist[i]->d_name, "..") != 0)
 				{
 					printf("\t%s\n", namelist[i]->d_name);
-					//free(namelist[i]);
 				}
 			}
-			//free(namelist);
 		}
 		closedir(d2);
 	}
@@ -107,7 +57,7 @@ int main(int argc, char* argv[]) {
 //////////////
 
 	//cria directório de restore (dir3)
-	int mkdir(const char *pathname, mode_t mode);
+	//int mkdir(const char *pathname, mode_t mode);
 	if((mkdir(dir3, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | EEXIST))==-1) {
 		perror(dir3);
 		exit(3);
@@ -147,7 +97,25 @@ int main(int argc, char* argv[]) {
 					exit(3);
 				}
 				if (S_ISREG(stat_buf.st_mode))
-					fileCopy(direntp->d_name, dir3);		
+				{
+					pid_t pid;
+					pid = fork();
+					if(pid==0) 
+					{
+						fileCopy(direntp->d_name, dir3);
+						printf("Restore: %s\n",direntp->d_name);
+						exit(0); 
+					}
+					else
+					{
+						int statloc;
+						wait(&statloc);
+						//QUESTION pai deve esperar?
+						//waitpid(pid, &statloc, WNOHANG); //QUESTION usar pid ou -1????
+						if(statloc==-1)
+							printf("Processo de copia terminou com erro!");
+					}					
+				}		
 			}
 
 			closedir(d4);
