a7d42953cdd0e3169b71924d5333283be631c97a
answersun <answersun@f10c690a-065e-4c8b-b573-574a2598cf45>
2013-04-21 00:55:32 +0000

    comentei o código, apaguei funcoes.h, como vai ser em 2 aplicações em pastas diferentes, não vale pena

--- trunk/rstr.c
+++ trunk/rstr.c
@@ -5,8 +5,46 @@
 #include <errno.h>
 #include <unistd.h>
 #include <wait.h>
-#include "funcoes.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h> //open files
+
+#define BUFFER_SIZE 1024
 //#define PATH_MAX 
+
+void fileCopy(char* path1, char* path2) {
+
+	int fd1, fd2, nr, nw;
+	unsigned char buffer[BUFFER_SIZE]; 
+
+	//abrir o ficheiro de origin onde quer copiar em mode de leitura
+	fd1 = open(path1, O_RDONLY); 
+	if (fd1 == -1) {
+		perror(path1); 
+		exit(5); 
+	}			
+	//criar e abrir o ficheiro onde vai ser escrito o contéudo do ficheiro do origin
+	fd2 = open(path2, O_WRONLY | O_CREAT | O_EXCL, 0644);
+	if (fd2 == -1) {
+		perror(path2); 
+		close(fd1);
+		exit(5);
+	} 
+	//processo de ler do ficheiro origin de tamanho BUFFER_SIZE de cada vez para escrever no ficheiro destino
+	while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0) 
+	{
+		if ((nw = write(fd2, buffer, nr)) <= 0 || nw != nr) { 
+			perror(path2); 
+			close(fd1); 
+			close(fd2); 
+			exit(6); 
+		}
+	}
+	//fechar os descriptores
+	close(fd1); 
+	close(fd2); 
+}
+
 
 DIR *d2, *d3;
 
@@ -15,35 +53,37 @@ char* dir3;
 
 int main(int argc, char* argv[]) {
 
-	dir2=argv[1];
-	dir3=argv[2];
+	dir2=argv[1];    //directório 1 (a fazer restore)
+	dir3=argv[2];    //directorio 2 (destino restore)
 
-	//teste utilização
+	//informação de utilização, caso utilizador não insira numeros dos parâmetros certos
 	if(argc!=3){
 		printf("usage: %s dir2 dir3\n",argv[0]);
 		exit(1);
 	}
 
-	struct dirent **namelist;
+	struct dirent **namelist;  
 	int n, i;
 	//abre directório (dir2) onde contem os backups todos e mostrar todos os backups que existem
 	if ((d2 = opendir(dir2)) != NULL)
 	{
+		//procura o diretorio de onde quer fazer restore e guardar todos os nomes dos couteudos na namelist
 		n = scandir(dir2, &namelist, NULL, alphasort);
 		if(n<0)
 			perror("scandir");
 		else
 		{
 			printf("List of avaliable restore points\n");
-			for(i=0; i<n; i++)
+			for(i=0; i<n; i++)   //imprime todos os restore points
 			{
+				//para não imprir "." e ".."
 				if((strcmp(namelist[i]->d_name, ".") != 0) && strcmp(namelist[i]->d_name, "..") != 0)
 				{
 					printf("\t%s\n", namelist[i]->d_name);
 				}
-				free(namelist[i]);
+				free(namelist[i]);  
 			}
-			free(namelist);
+			free(namelist);  //libertar a memoria foi colocada para namelist
 		}
 		closedir(d2);
 	}
@@ -53,9 +93,23 @@ int main(int argc, char* argv[]) {
 		exit(4);
 	}
 
+	char op_dir[BUFFER_SIZE];
+	printf("\nWhich restore point (time) ?\n");
+
+
+	int b = 1;
+	while(b!=0)   //controlo de inserção do nome da pasta que quer fazer restore 
+	{
+		scanf("%s", op_dir);
+		char path1[PATH_MAX];
+		sprintf(path1, "%s/%s", dir2,op_dir);			
+		b=chdir(path1);
+		if(b!=0)
+			printf("Restore point doesn't exist, try again!\n");
+	}
+
 
 	//cria directório de restore (dir3)
-	//int mkdir(const char *pathname, mode_t mode);
 	if((mkdir(dir3, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH | EEXIST))==-1) {
 		perror(dir3);
 		exit(3);
@@ -65,23 +119,7 @@ int main(int argc, char* argv[]) {
 		perror(dir3);
 		exit(2); 
 	}
-	
 
-	char op_dir[BUFFER_SIZE];
-	printf("\nWhich restore point (time) ?\n");
-	
-
-	int b = 1;
-	while(b!=0)
-	{
-		scanf("%s", op_dir);
-		char path1[PATH_MAX];
-		sprintf(path1, "%s/%s", dir2,op_dir);			
-		b=chdir(path1);
-		if(b!=0)
-			printf("Restore point doesn't exist, try again!\n");
-	}
-	
 	FILE *fp;
 	char *filename = NULL;
 	char *datemodi = NULL;
@@ -89,7 +127,8 @@ int main(int argc, char* argv[]) {
 	ssize_t read;
 	size_t len = 0;
 	char pathorg[PATH_MAX], pathdes[PATH_MAX];
-	
+
+	//abrir o ficheiro __bckpinfo__ em mode de leitura
 	fp = fopen("__bckpinfo__", "r");
 
 	if (fp == NULL)
@@ -97,31 +136,37 @@ int main(int argc, char* argv[]) {
 
 	int n_filhos = 0;
 	while(!feof(fp))
-	{			
+	{	
+		//1+3*n (n=0,1,2...) linha: nome do ficheiro		
 		if((read = getline(&filename, &len, fp)) == -1) {
 			break;
 		}
 		if (filename[read-1] == '\n')
 			filename[read-1] = '\0';
+
+		//2+3*n (n=0,1,2...) linha: data da última alteração do ficheiro	
 		if((getline(&datemodi, &len, fp)) == -1) {
 			perror("__bckpinfo__-datemodif");
 		}
+
+		//3+3*n (n=0,1,2...) linha: nome da pasta onde está o ficheiro da linha 1+3*n
 		if((read = getline(&pathname, &len, fp)) == -1) {
 			perror("__bckpinfo__-pathname");
 		}
 		if (pathname[read-1] == '\n')
 			pathname[read-1] = '\0';
-		
+
 		sprintf(pathorg, "%s/%s/%s", dir2, pathname, filename);
 		sprintf(pathdes, "%s/%s", dir3, filename);
 
 		pid_t pid;
 		pid = fork();
 		n_filhos++;
+		//processo filho
 		if(pid==0) 
 		{
 			printf("Restore: %s\n",filename);
-			fileCopy(pathorg, pathdes);				
+			fileCopy(pathorg, pathdes);		//processo de copiar ficheiros		
 			exit(0); 
 		}
 		else
@@ -130,7 +175,7 @@ int main(int argc, char* argv[]) {
 			while(i--)
 			{
 				int statloc;
-				pid_t p = waitpid(-1, &statloc, WNOHANG);
+				pid_t p = waitpid(-1, &statloc, WNOHANG);  //receber o estado de terminação de qualquer processo filho sem bloquer
 				if(p > 0)
 					n_filhos--;
 				if(p < 0 || statloc==-1)
@@ -139,10 +184,12 @@ int main(int argc, char* argv[]) {
 
 		}
 	}
+	//libertar memoria alocadas para filename, datemodi e pathname
 	free(filename);
 	free(datemodi);
 	free(pathname);
 
+	//receber o estado de terminação de todos os restos filhos não foram apanhados pelo waitpid, para não ficar nenhum processo no estado zombie
 	while(n_filhos--)
 	{
 		int statloc;
